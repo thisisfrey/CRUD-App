a63adc81605ed6c4ecc57d44528457d73b61eab2
Antonia Frey <antonia.frey@devoteam.com>
2023-07-21 23:05:45 +0200

    Added dialog component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddEditEmployeeComponent } from './add-edit-employee/add-edit-employee.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ng-crud-app';
+
+  constructor(private _dialog: MatDialog) {}
+
+  openAddEditEmployeeForm() {
+    this._dialog.open(AddEditEmployeeComponent);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { AddEditEmployeeComponent } from './add-edit-employee/add-edit-employee.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AddEditEmployeeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -18,6 +22,9 @@ import { MatButtonModule } from '@angular/material/button';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
