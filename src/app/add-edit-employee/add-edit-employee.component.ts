abed2932ec1f9a9b63e429a3d2117e875708dae6
Antonia Frey <antonia.frey@devoteam.com>
2023-07-22 09:56:31 +0200

    Completed form

--- src/app/add-edit-employee/add-edit-employee.component.ts
+++ src/app/add-edit-employee/add-edit-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-edit-employee',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-edit-employee.component.scss'],
 })
 export class AddEditEmployeeComponent {
+  employeeForm: FormGroup;
+
   education: string[] = ['Intermediate', 'Diploma', 'Graduate', 'Other'];
+
+  constructor(private _fb: FormBuilder) {
+    this.employeeForm = this._fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      dob: '',
+      gender: '',
+      education: '',
+      company: '',
+      experience: '',
+      package: '',
+    });
+  }
+
+  onFormSubmit(){
+    if(this.employeeForm.valid){
+      console.log(this.employeeForm.value)
+    }
+  }
 }
