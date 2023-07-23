801ab19e8cdaff578a6498e3f7424821da2e78a7
Antonia Frey <antonia.frey@devoteam.com>
2023-07-23 15:49:31 +0200

    Added server urls

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { SERVER_URL } from 'src/server_url';
 
 @Injectable({
   providedIn: 'root',
@@ -12,18 +13,18 @@ export class EmployeeService {
   // ? Type Observable<any>, TODO: Replace type any
   // Service method can be called from component
   addEmployee(data: IEmployee): Observable<any> {
-    return this._http.post('http://localhost:3000/employees', data);
+    return this._http.post(`${SERVER_URL}/employees`, data);
   }
 
   getEmployees(): Observable<any> {
-    return this._http.get('http://localhost:3000/employees');
+    return this._http.get(`${SERVER_URL}/employees`);
   }
 
   deleteEmployee(id: number): Observable<any> {
-    return this._http.delete(`http://localhost:3000/employees/${id}`);
+    return this._http.delete(`{${SERVER_URL}/employees/${id}`);
   }
 
   updateEmployee(id: number, data: IEmployee): Observable<any> {
-    return this._http.put(`http://localhost:3000/employees/${id}`, data);
+    return this._http.put(`{${SERVER_URL}/employees/${id}`, data);
   }
 }
---
+++ src/server_url.ts
@@ -0,0 +1,8 @@
+// JSON server running locally after starting
+// Start with: json-server --watch db.json
+export const SERVER_URL = 'http://localhost:3000';
+
+// Server online: https://my-json-server.typicode.com/
+//my-json-server.typicode.com/thisisfrey/json-server
+/* export const SERVER_URL =
+  'https://my-json-server.typicode.com/thisisfrey/json-server/'; */
